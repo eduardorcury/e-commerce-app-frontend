d997c3d435c9ac24fef1e4d07ab612b681ffefc0
eduardorcury <eduardoribeirocury@gmail.com>
2020-05-21 19:05:15 -0400

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "https://curso-spring-eduardo.herokuapp.com"
+    baseUrl: "https://curso-spring-eduardo.herokuapp.com",
+    bucketBaseUrl: "https://curso-spring-edu.s3-sa-east-1.amazonaws.com"
 }
