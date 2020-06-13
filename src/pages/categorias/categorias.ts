a22a34db20461cb53f69fa4bed75639652110442
eduardorcury <eduardoribeirocury@gmail.com>
2020-06-12 22:37:35 -0400

    Component loading nas páginas de categoria e produtos

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
 import { CategoriaDTO } from '../../models/categoria.dto';
 import { API_CONFIG } from '../../config/api.config';
@@ -18,20 +18,33 @@ export class CategoriasPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public categoriaService: CategoriaService) {
+    public categoriaService: CategoriaService,
+    public loadingControl: LoadingController) {
   }
 
   ionViewDidLoad() {
 
+    let loader = this.presentLoading();
     this.categoriaService.findAll()
       .subscribe(response => {
         this.items = response;
+        loader.dismiss();
       },
-      error => {});
+      error => {
+        loader.dismiss();
+      });
     console.log();
   }
 
   showProdutos(categoria_id: string) {
     this.navCtrl.push("ProdutosPage", {categoria_id: categoria_id});
+  }
+
+  presentLoading() {
+    let loader = this.loadingControl.create({
+      content: "Aguarde...",
+    });
+    loader.present();
+    return loader;
   }
 }
